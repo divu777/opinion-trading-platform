d85b959836ebe7cb24aba4fb934e7a7a690ca5a2
Divakar Jaiswal <divakarjaiswal707@gmail.com>
2025-08-29 20:07:26 +0530

    Changed next config

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  "allowedDevOrigins":['134.209.156.168']
 };
 
 export default nextConfig;
